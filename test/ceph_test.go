3ae8a76b473b662eb55c8ee2c51a44204e2f1e34
Weiyanyu <chinayeonon@gmail.com>
2020-03-07 17:36:16 +0800

    delete file from ceph

--- test/ceph_test.go
+++ test/ceph_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"fmt"
 	"go-disk/store/ceph"
+	"log"
 	"testing"
 )
 
@@ -14,5 +15,22 @@ func TestConn(t *testing.T) {
 		fmt.Printf("err : %v", err)
 		return
 	}
-	fmt.Println(res.Contents)
+
+	fmt.Println(len(res.Contents))
+}
+
+func TestDeleteAllFile(t *testing.T) {
+	bucket := ceph.GetCephBucket("files")
+
+	res, err := bucket.List("", "", "", 10)
+	if err != nil {
+		fmt.Printf("err : %v", err)
+		return
+	}
+
+	for _, content := range res.Contents {
+		log.Println(content.Key)
+		bucket.Del(content.Key)
+	}
+
 }
